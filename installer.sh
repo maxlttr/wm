95e053e16e0f57e7f8341960c6a9be5e9bce0b75
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-05-26 18:34:22 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
 
+sudo pacman -S git neofetch htop vim networkmanager nm-connection-editor otf-aurulent-nerd firefox qtile polybar
+git clone https://github.com/maxlttr/wm.git
+
+#Polybar
+mkdir ~/.config/polybar/
+mv ~/wm/config.ini ~/.config/polybar/config.ini
+
+#Qtile
+cp -f ~/wm/config.py ~/.config/qtile/config.py
+rm ~/wm/config.py
+mv ~/wm/autostart.sh ~/.config/qtile/autostart.sh
+chmod +x autostart.sh
+
+#Wallpaper
+mv ~/wm/image.jpg ~/.config/qtile/image.jpg
+
+
+
