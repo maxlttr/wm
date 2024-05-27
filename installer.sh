d628c92522ba5ec5c1e831015a78edbcef343810
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-05-27 22:50:31 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,16 +7,15 @@ git clone https://github.com/maxlttr/wm.git
 
 #Polybar
 mkdir ~/.config/polybar/
-mv ~/Downloads/wm/config.ini ~/.config/polybar/config.ini
+cp -i ~/Downloads/wm/config.ini ~/.config/polybar/config.ini
 
 #Qtile
-cp -f ~/Downloads/wm/config.py ~/.config/qtile/config.py
-rm ~/Downloads/wm/config.py
+cp -i ~/Downloads/wm/config.py ~/.config/qtile/config.py
 chmod +x ~/Downloads/wm/autostart.sh
-mv ~/Downloads/wm/autostart.sh ~/.config/qtile/autostart.sh
+cp -i ~/Downloads/wm/autostart.sh ~/.config/qtile/autostart.sh
 
 #Wallpaper
-mv ~/Downloads/wm/image.jpg ~/.config/qtile/image.jpg
+cp -i ~/Downloads/wm/image.jpg ~/.config/qtile/image.jpg
 
 #Clean useless file and folder
 rm -r ~/Downloads/wm/ && rm installer.sh
