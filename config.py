9654c89d8576da3c9437c023f67a80c1c4ca1346
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-05-28 11:32:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -138,7 +138,7 @@ layouts = [
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    layout.MonadTall(border_focus="cc99ff", border_width=5, margin=25),
+    layout.MonadTall(border_focus="cc99ff", border_width=5, margin=15),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
