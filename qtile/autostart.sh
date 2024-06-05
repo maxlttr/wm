5d8c59f0af049b1173b62dd17d431eab4b916a8d
maxlttr_ <168470807+maxlttr@users.noreply.github.com>
2024-06-05 12:14:51 +0200

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 polybar &
-picom -b --config=/dev/null --fading --fade-in-step=0.01 --fade-out-step=0.01 --fade-delta=3 --inactive-opacity=0.7 --corner-radius=10 --vsync --blur-method=dual_kawase --blur-background-fixed --blur-strength=4 --backend=glx --glx-no-stencil
+picom -b --config=/dev/null --fading --fade-in-step=0.01 --fade-out-step=0.01 --fade-delta=3 --inactive-opacity=0.8 --corner-radius=10 --vsync --blur-method=dual_kawase --blur-background-fixed --blur-strength=6 --backend=glx --glx-no-stencil
 sxhkd &
 nitrogen --set-zoom-fill --random ~/.config/wallpapers/ &
